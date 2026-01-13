7bc3c83220179f456f211f4f13bc9d1cfd08e226
Borja <bdesaavazquez@danielcastelao.org>
2026-01-13 09:37:58 +0100

    Tarea37 finalizada

--- src/Tarea37/Tarea37.java
+++ src/Tarea37/Tarea37.java
@@ -1,4 +1,40 @@
 package Tarea37;
 
+import java.util.Scanner;
+
 public class Tarea37 {
-}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+        System.out.print("Introduce el texto: ");
+        String texto = sc.nextLine().toUpperCase();
+
+        System.out.print("Introduce el desplazamiento: ");
+        int desplazamiento = sc.nextInt();
+
+        String resultado = "";
+
+        for (int i = 0; i < texto.length(); i++) {
+            char caracter = texto.charAt(i);
+
+            int posicionActual = alfabeto.indexOf(caracter);
+
+            if (posicionActual != -1) {
+                int nuevaPosicion = (posicionActual + desplazamiento) % 27;
+
+                if (nuevaPosicion < 0) nuevaPosicion += 27;
+
+                resultado += alfabeto.charAt(nuevaPosicion);
+                } else {
+                    resultado += caracter;
+                }
+            }
+
+        System.out.println("Texto cifrado: " + resultado);
+        sc.close();
+        }
+    }
